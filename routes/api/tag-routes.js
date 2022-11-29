6ad2a9e3d48ede0092998e80c82118911b65b9cd
BigSev15 <palat008@umn.edu>
2022-11-29 11:46:30 -0800

    wrote get single product by id route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -19,6 +19,20 @@ router.get('/', async (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product, through: ProductTag, as: 'tags_for_product'}]
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with this id.' });
+      return;
+    }
+    
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
