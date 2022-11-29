8ce085d275c532fcaf8dc6bd2dc70b74f75f478a
BigSev15 <palat008@umn.edu>
2022-11-29 12:10:45 -0800

    wrote get all categories route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -53,6 +53,12 @@ router.put('/:id', async (req, res) => {
         id: req.params.id
       },
     });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+    
     res.status(200).json(tagData);
   } catch (err) {
     res.status(400).json(err);
